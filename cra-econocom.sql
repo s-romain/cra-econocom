92ce910a0c1f6e4f3950853652135baaf28f07f0
Aloïs HIAS <aloishias@MacBook-Pro-de-Alois.local>
2022-01-10 16:31:00 +0100

    Ajout du model de données

---
+++ cra-econocom.sql
@@ -0,0 +1,49 @@
+CREATE SCHEMA cra_e;
+
+CREATE TABLE cra_e.accounts (
+  id_account INT GENERATED ALWAYS AS IDENTITY,
+  first_name_account VARCHAR(30),
+  last_name_account VARCHAR(30),
+  login_account VARCHAR(30),
+  password_account VARCHAR(30),
+  PRIMARY KEY (accountId)
+);
+
+CREATE TABLE cra_e.task (
+  id_task INT GENERATED ALWAYS AS IDENTITY,
+  id_manager INT,
+  name_task VARCHAR(30),
+  duration_task INT,
+  type_task VARCHAR(30),
+  PRIMARY KEY (id_task),
+  CONSTRAINT fkAccount
+      FOREIGN KEY(id_manager) 
+	  REFERENCES cra_e.accounts(id_account)
+);
+
+CREATE TABLE cra_e.made_work (
+    id_made_work INT GENERATED ALWAYS AS IDENTITY,
+    duration_made_work INT,
+    parrent_work_made_work INT,
+    date_of_work_made_work INT,
+    id_account_made_work INT,
+    id_task_made_work INT,
+    PRIMARY KEY (id_made_work),
+    CONSTRAINT fkAccount
+        FOREIGN KEY(id_account_made_work) 
+        REFERENCES cra_e.accounts(id_account),
+    CONSTRAINT fkTask
+        FOREIGN KEY(id_task_made_work) 
+        REFERENCES cra_e.task(id_task)
+)
+
+
+create role postgrestRole nologin;
+
+grant usage on schema montreal to postgrestRole;
+grant all on cra_e.accounts to postgrestRole;
+grant all on cra_e.made_work to postgrestRole;
+grant all on cra_e.task to postgrestRole;
+
+create role authenticator noinherit login password 'root';
+grant postgrestRole to authenticator;
