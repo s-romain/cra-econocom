02bf248c2932b061eabce1b3496c9b4a5aa02eaa
Aloïs HIAS <aloishias@macbook-pro-de-alois.home>
2022-01-12 10:23:41 +0100

    Update again error in script

--- cra-econocom.sql
+++ cra-econocom.sql
@@ -41,8 +41,8 @@ create role postgrestRole nologin;
 
 grant usage on schema cra_e to postgrestRole;
 grant all on cra_e.accounts to postgrestRole;
-grant all on cra_e.made_work to postgrestRole;
-grant all on cra_e.task to postgrestRole;
+grant all on cra_e.made_works to postgrestRole;
+grant all on cra_e.tasks to postgrestRole;
 
 create role authenticator noinherit login password 'root';
 grant postgrestRole to authenticator;
