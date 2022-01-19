0dc71ca642ea48a91dba70c3dfe3a0b2caf7d106
aloishias <hias.alois@gmail.com>
2022-01-19 11:42:59 +0100

    Add function to sql script

--- cra-econocom.sql
+++ cra-econocom.sql
@@ -49,18 +49,26 @@ grant all on cra_e.tasks to postgrestRole;
 create role authenticator noinherit login password 'root';
 grant postgrestRole to authenticator;
 
-CREATE OR REPLACE function made_work_by_account(idAccount integer) returns table as $$
-declare 
-	made_work_by_account table;
+
+--Fonction pour requêtea avec inner join
+CREATE OR REPLACE function cra_e.made_work_by_account(idAccount integer) 
+returns table(
+	name_task varchar,
+	duration_made_work integer,
+	duration_task integer,
+	date_of_work_made_work integer
+	) 
+as $$
 begin
-  SELECT tasks.name_task, made_works.duration_made_work, tasks.duration_task, made_works.date_of_work_made_work
-  INTO made_work_by_account
+  return query SELECT 
+  	tasks.name_task, 
+  	made_works.duration_made_work, 
+  	tasks.duration_task, 
+  	made_works.date_of_work_made_work
   FROM cra_e.made_works
   INNER JOIN cra_e.tasks 
     ON tasks.id_task = made_works.id_task_made_work
   WHERE made_works.id_account_made_work = idAccount;
-
-  RETURN made_work_by_account;
 end;
 $$
-language plpgsql immutable;
+language plpgsql;
