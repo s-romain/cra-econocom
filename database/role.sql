704880d847bdfb2bdfc790cd5fb597126c4dbf6d
Aloïs HIAS <aloishias@macbook-pro-de-alois.home>
2022-01-21 16:35:17 +0100

    Update schema and role

--- database/role.sql
+++ database/role.sql
@@ -2,8 +2,11 @@ create role postgrestRole nologin;
 
 grant usage on schema cra_e to postgrestRole;
 grant all on cra_e.accounts to postgrestRole;
-grant all on cra_e.made_works to postgrestRole;
+grant all on cra_e.project_informations to postgrestRole;
 grant all on cra_e.tasks to postgrestRole;
+grant all on cra_e.us_os to postgrestRole;
+grant all on cra_e.weekly_reports to postgrestRole;
+grant all on cra_e.monthly_reports to postgrestRole;
 
 create role authenticator noinherit login password 'root';
 grant postgrestRole to authenticator;
--- database/schema.sql
+++ database/schema.sql
@@ -12,7 +12,8 @@ CREATE TABLE cra_e.accounts (
 
 CREATE TABLE cra_e.project_informations (
     id_project INT GENERATED ALWAYS AS IDENTITY,
-    name_project VARCHAR(30)
+    name_project VARCHAR(30),
+    PRIMARY KEY (id_project)
 );
 
 CREATE TABLE cra_e.tasks (
@@ -35,12 +36,11 @@ CREATE TABLE cra_e.us_os (
     us_name VARCHAR(30),
     os_name VARCHAR(30),
     date_of_work_made_work INT,
+    PRIMARY KEY (id_us_os),
     CONSTRAINT fkPojectInformation
         FOREIGN KEY(id_project_information) 
         REFERENCES cra_e.project_informations(id_project)
 );
-
-
 
 CREATE TABLE cra_e.weekly_reports (
     id_weekly_report INT GENERATED ALWAYS AS IDENTITY,
@@ -48,6 +48,7 @@ CREATE TABLE cra_e.weekly_reports (
     id_task INT,
     duration_weekly_report VARCHAR(30),
     state_weekly_report VARCHAR(30),
+    PRIMARY KEY (id_weekly_report),
     CONSTRAINT fkAccount
         FOREIGN KEY(id_account) 
         REFERENCES cra_e.accounts(id_account),
@@ -61,6 +62,7 @@ CREATE TABLE cra_e.monthly_reports (
     id_account INT,
     id_task INT,
     date_monthly_reports INT,
+    PRIMARY KEY (id_monthly_report),
     CONSTRAINT fkAccount
         FOREIGN KEY(id_account) 
         REFERENCES cra_e.accounts(id_account),
